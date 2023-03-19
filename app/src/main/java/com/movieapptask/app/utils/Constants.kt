9b0017f5cc0f3ab95c371656a26c63a347f9a79e
Mohamed <mohamedfawz2010@gmail.com>
2023-03-19 16:07:40 +0200

    mm

--- app/src/main/java/com/movieapptask/app/utils/Constants.kt
+++ app/src/main/java/com/movieapptask/app/utils/Constants.kt
@@ -2,7 +2,6 @@ package com.movieapptask.app.utils
 
 object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
-//    const val API_KEY = "d2f6821903e6344eb4737e5886da40bd"
     const val KEY_ID = "id"
     const val account_id = "17689561"
     const val session_id = "31098355a71e3363ad2b175561e54dac88d251da"
